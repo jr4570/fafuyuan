ddbcdb967e3df8afddf5cabb99bfa5c589a86b08
jr4570 <98681506+jr4570@users.noreply.github.com>
2024-10-19 14:13:11 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -97,7 +97,7 @@ export default defineConfig({
                 footer: {
                   selectText: '选择',
                   navigateText: '切换',
-                  toCloseText: '关闭',
+                  tocloseText: '关闭',
                 }
               }
             }
